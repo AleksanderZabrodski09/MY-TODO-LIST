e55ac5a93bb3a048c3a9d73ad41b349af3545978
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-06-26 22:59:15 +0200

    create: add tasks.map and removeTask(2-les)

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 type TodoListType = {
-  title:string
-  task: TaskType[]
+  title: string
+  tasks: TaskType[]
+  removeTask: (id: number) => void
 }
 
 type TaskType = {
@@ -21,9 +22,16 @@ export const TodoList = (props: TodoListType) => {
         <button>+</button>
       </div>
       <ul>
-        <li><input type="checkbox" checked={props.task[0].isDone}/> <span>{props.task[0].title}</span></li>
-        <li><input type="checkbox" checked={props.task[1].isDone}/> <span>{props.task[1].title}</span></li>
-        <li><input type="checkbox" checked={props.task[2].isDone}/> <span>{props.task[2].title}</span></li>
+        {props.tasks.map((el) => {
+          return (
+            <li key={el.id}>
+              <input type="checkbox" checked={el.isDone}/>
+              <span>{el.title}</span>
+              <button onClick={() => {props.removeTask(el.id)}}>✖</button>
+            </li>
+          )
+        })
+        }
       </ul>
       <div>
         <button>All</button>
