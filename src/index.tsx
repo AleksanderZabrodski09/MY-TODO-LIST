73a8602eae738367119c53b489817c2f3addfd01
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-12-23 13:57:46 +0100

    create: add AppWithReducer and add useReducer (10)

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AppWithReducer from './AppWithReducer';
 
 // ReactDOM.render(<App />,  document.getElementById('root'));
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(<AppWithReducer />);
 
 
 
