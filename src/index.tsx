88aa6ec91737684cd0071a295be05b3a16bb1b8e
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-12-23 14:30:54 +0100

    create: add tag Provider (10)

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import {createRoot} from 'react-dom/client';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import AppWithReducer from './AppWithReducer';
+import {Provider} from 'react-redux';
+import {store} from './state/store';
 
 // ReactDOM.render(<App />,  document.getElementById('root'));
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<AppWithReducer />);
-
+root.render(
+  <Provider store={store}>
+    <AppWithReducer/>
+  </Provider>);
 
 
 // If you want your app to work offline and load faster, you can change
