979f971c516a7076c65195fc342171440f1479fd
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-01-03 21:39:52 +0100

    create: add refactoring AppWithRedux.stories.tsx (12)

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -3,6 +3,7 @@ import {ComponentMeta, ComponentStory} from '@storybook/react';
 import AppWithRedux from '../AppWithRedux';
 import {Provider} from 'react-redux';
 import {store} from '../state/store';
+import {ReduxStoreProviderDecorator} from './ ReduxStoreProviderDecorator';
 
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -15,10 +16,11 @@ export default {
   //     description: 'Button clicked'
   //   },
   // },
+  decorators:[ReduxStoreProviderDecorator]
 } as ComponentMeta<typeof AppWithRedux>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof AppWithRedux> = (args) => <Provider store={store}><AppWithRedux/></Provider>;
+const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux/>;
 
 export const AppWithReduxStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
