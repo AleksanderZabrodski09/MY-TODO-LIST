9f8ab332d02022b45c6310e9b271bea8189b38be
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-02-13 18:06:27 +0100

    create: add getTask in todolist-api.ts (14)

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -9,7 +9,7 @@ const settings = {
 }
 
 const instance = axios.create({
-  baseURL:'https://social-network.samuraijs.com/api/1.1/',
+  baseURL: 'https://social-network.samuraijs.com/api/1.1/',
   withCredentials: true,
   headers: {
     'API-KEY': '5983a517-e387-4f12-ac5e-a15dd0cef3b7'
@@ -22,9 +22,9 @@ export const todolistAPI = {
     return instance.get<TodolistType[]>('todo-lists')
       .then((res) => res.data)
   },
-  createTodolist(title:string) {
+  createTodolist(title: string) {
     // let title = 'TS-JS-REACT'
-    return instance.post<ResponseType<{item:TodolistType}>>(`todo-lists`, {title})
+    return instance.post<ResponseType<{ item: TodolistType }>>(`todo-lists`, {title})
       .then((res) => res.data)
   },
   deleteTodolist(todolistId: string,) {
@@ -32,45 +32,83 @@ export const todolistAPI = {
     return instance.delete<ResponseType>(`todo-lists/${todolistId}`)
       .then((res) => res.data)
   },
-  updateTodolist(todolistId: string,title:string) {
+  updateTodolist(todolistId: string, title: string) {
 
-    return  instance.put<ResponseType>(`todo-lists/${todolistId}`, {title})
+    return instance.put<ResponseType>(`todo-lists/${todolistId}`, {title})
+      .then((res) => res.data)
+  },
+  getTasks(todolistId:string) {
+    return instance.get<getTasksResponse>(`todolists/${todolistId}/tasks`)
       .then((res) => res.data)
   }
 }
 
+type getTasksResponse = {
+  totalCount: number
+  error: string
+  items: TaskType[]
+}
 
-  type TodolistType  = {
-    id: string
-    title:  string
-    addedDate: string
-    order: number
-  }
+export enum TaskPriorities {
+  Low = 0,
+  Middle = 1,
+  Hi = 2,
+  Urgently = 3,
+  Later = 4
+}
 
-  type createTodolistType ={
-    fieldsErrors: string[]
-    messages: string[]
-    resultCode: number
-    data: {
-      item:  TodolistType
-    }
-  }
-  //   type updateTodolistType ={
-  //   fieldsErrors: string[]
-  //   messages: string[]
-  //   resultCode: number
-  //   data: {}
-  // }
-  //   type deleteTodolistType ={
-  //   fieldsErrors: string[]
-  //   messages: string[]
-  //   resultCode: number
-  //   data: {}
-  // }
+export enum TaskStatuses {
+  New = 0,
+  InProgress = 1,
+  Completed = 2,
+  Draft = 3
+}
 
-  type ResponseType<D = {}>={
-    fieldsErrors: string[]
-    messages: string[]
-    resultCode: number
-    data: D
-  }
+export type TaskType = {
+  description: string
+  title: string
+  // completed: boolean
+  status: TaskStatuses
+  priority: TaskPriorities
+  startDate: string
+  deadline: string
+  id: string
+  todoListId: string
+  order: number
+  addedDate: string
+}
+
+export type TodolistType = {
+  id: string
+  title: string
+  addedDate: string
+  order: number
+}
+//
+// type createTodolistType ={
+//   fieldsErrors: string[]
+//   messages: string[]
+//   resultCode: number
+//   data: {
+//     item:  TodolistType
+//   }
+// }
+//   type updateTodolistType ={
+//   fieldsErrors: string[]
+//   messages: string[]
+//   resultCode: number
+//   data: {}
+// }
+//   type deleteTodolistType ={
+//   fieldsErrors: string[]
+//   messages: string[]
+//   resultCode: number
+//   data: {}
+// }
+
+type ResponseType<D = {}> = {
+  fieldsErrors: string[]
+  messages: string[]
+  resultCode: number
+  data: D
+}
