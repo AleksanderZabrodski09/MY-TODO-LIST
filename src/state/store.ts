01667b2eadc37c79825ed3e23968720dc86fa444
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-02-15 21:01:10 +0100

    create: add AppDispatch and useAppSelector(14)

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,16 +1,25 @@
 import {todolistsReducer} from './todolists-reducer';
-import {combineReducers, legacy_createStore} from 'redux';
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux';
 import {tasksReducer} from './tasks-reducer';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 
 
-export type AppRootReducerType =ReturnType<typeof rootReducer>
+
 
 const rootReducer = combineReducers({
   tasks:tasksReducer,
   todolists: todolistsReducer
 })
 
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
+
+export type AppRootReducerType =ReturnType<typeof rootReducer>
+
+type AppDispatchType = ThunkDispatch<AppRootReducerType, any, AnyAction >
+export const AppDispatch=()=>useDispatch<AppDispatchType>()
+
+export const useAppSelector:TypedUseSelectorHook<AppRootReducerType>=useSelector
 
 // @ts-ignore
 window.store = store
