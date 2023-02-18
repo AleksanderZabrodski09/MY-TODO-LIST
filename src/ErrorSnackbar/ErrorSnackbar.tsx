4153a0d7fd4810c2cd83ede318049a46b243d0ee
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-02-18 11:10:37 +0100

    create:  add actionCreators for status and error  (15)

---
+++ src/ErrorSnackbar/ErrorSnackbar.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, {AlertProps} from '@mui/material/Alert';
+import {AppRootReducerType, useAppSelector} from '../state/store';
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref,
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export const ErrorSnackbar = () => {
+  // const [open, setOpen] = React.useState(true);
+
+  // const handleClick = () => {
+  //   setOpen(true);
+  // };
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    // setOpen(false);
+  };
+const error= useAppSelector<string|null>(state => state.app.error)
+const isOpen=error !==null
+  return (
+    <Snackbar open={isOpen} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
+        This is an Error message 😠!
+      </Alert>
+    </Snackbar>
+  )
+}
